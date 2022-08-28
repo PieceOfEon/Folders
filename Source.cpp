2463135b3de9a3fba0fd290ec20f25a668119b26
PieceOfEon <danilskripka1996@gmail.com>
2022-08-28 13:54:25 +0300

    End

--- Source.cpp
+++ Source.cpp
@@ -18,7 +18,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include<vector>
-
+#include<conio.h>
 
 using namespace std;
 
@@ -29,88 +29,14 @@ private:
     string delname;
     string addnametxt;
     
-    
- /*   struct _finddata_t {
-        unsigned    attrib;
-        time_t      time_create;
-        time_t      time_access;
-        time_t      time_write;
-        _fsize_t    size;
-        char        name[260];
-    };*/
 public:
     
-
-    void searchpath(const char* file)
-    {
-        
-       /* char* res;*/
-
-       /* res = searchpath(file);*/
-
-        if (strlen(file) == 0)
-            cout << file << " not found\n";
-        else
-            cout << file << " found in %s\n"; /*printf("DMC.EXE found in %s\n", file);*/
-       /* return res;*/
-        /*return;*/
-    }
-
-    void CreateTxt()
-    {
-        cout << "Enter folder name: ";
-        cin >> addnametxt;
-        string path = "\\" + addname+".txt";
-        char* p = _strdup(path.c_str());
-
-        if (_mkdir(p) == 0);
-        {
-            cout << "Directory " << p << " was successfully created\n";
-            string dir = "dir ";
-            dir += path;
-            char* q = _strdup(dir.c_str());
-            system(q);
-        }
-    }
-
-   void Inform()
-    {
-       /*setlocale(LC_ALL, "");
-       HANDLE search_file;
-       WIN32_FIND_DATA aa;
-       search_file = FindFirstFile("C:\\*", &aa);
-       while (FindNextFile(search_file, &aa) != NULL)
-       {
-           cout << aa.cFileName << "\n";
-       }*/
-        /*std::string path = "путь к папке";
-        GetFileAttributes
-        for (const auto& entry : FileIdInfo()::directory_iterator(path))
-            std::cout << entry.path() << std::endl;*/
-       /* string infpapka;
-        cout << "Enter folder name getInf: ";
-        cin >> infpapka;
-        string path = "\\" + infpapka;
-       
-        char* q = _strdup(infpapka.c_str());
-        system(q);*/
-        /*if (_mkdir(p) == 0);
-        {
-            cout << "Directory " << p << " was successfully created\n";
-            string dir = "dir ";
-            dir += path;
-            char* q = _strdup(dir.c_str());
-            system(q);
-
-        }*/
-
-    }
-
     void DelPapka()
     {
-        cout << "Enter folder name: ";
+        cout << "for example ->>D:\\Test\tor\t->>D:Test\\Test\n";
+        cout << "Enter path for folder del: ";
         cin >> delname;
-        string path = "\\" + delname;
+        string path = "" + delname;
         char* p = _strdup(path.c_str());
         if (_rmdir(p) == 0)
         {
@@ -118,58 +44,26 @@ public:
         }
         else
         {
-            cout << "Problem removing directory " << p << ", maybe she's missing\n";
+            cout << "Problem removing directory " << p << ", maybe it's missing\n";
         }
-
     }
 
-    void RenameFolder()
+    void Rename()
     {
-        char old_name[] = "D:\\jjj";
-
-        char new_name[] = "D:\\222jjj";
-        int value;
-
-        // File name is changed here
-        value = rename(old_name, new_name);
-
-        // Print the result
-        if (!value)
-        {
-            printf("%s", "File name changed successfully");
-        }
+        cout << "For example D:\\222jjj   to D:\\222\n";
+        cout << "Enter path and oldname: ";
+        char oldname[50];
+        cin >> oldname;
+        cout << "Enter path and new name: ";
+        char newname[50];
+        cin >> newname;
+        /*	Deletes the file if exists */
+        if (rename(oldname, newname) != 0)
+            perror("Error renaming file");
         else
-        {
-            perror("Error");
-        }
+            cout << "File renamed successfully";
+
     }
-
-    void Search2(int argc, TCHAR* argv[])
-    {
-        WIN32_FIND_DATA FindFileData;
-        HANDLE hFind;
-
-        if (argc != 2)
-        {
-            _tprintf(TEXT("Usage: %s [target_file]\n"), argv[0]);
-            return;
-        }
-
-        _tprintf(TEXT("Target file is %s\n"), argv[1]);
-        hFind = FindFirstFile(argv[1], &FindFileData);
-        if (hFind == INVALID_HANDLE_VALUE)
-        {
-            printf("FindFirstFile failed (%d)\n", GetLastError());
-            return;
-        }
-        else
-        {
-            _tprintf(TEXT("The first file found is %s\n"),
-                FindFileData.cFileName);
-            FindClose(hFind);
-        }
-    }
-    
     void Disk2()
     {
         DWORD Disks = GetLogicalDrives();
@@ -188,9 +82,10 @@ public:
 
     void CreatePapka()
     {
-        cout << "Enter folder name: ";
+        cout << "for example ->>D:\\Test\tor\t->>D:Test\\Test\n";
+        cout << "Enter path for create: ";
         cin >> addname;
-        string path = "\\" + addname;
+        string path = "" + addname;
         char* p = _strdup(path.c_str());
 
         if (_mkdir(p) == 0);
@@ -200,17 +95,16 @@ public:
             dir += path;
             char* q = _strdup(dir.c_str());
             system(q);
-           
         }
     }
 
-    void my_search_tree(string dir, string mask, unsigned int depth) {
+    void Search(string a, string m, unsigned int n) {
 
         WIN32_FIND_DATAA d;
 
-        if (dir.empty() || mask.empty()) return;
+        if (a.empty() || m.empty()) return;
 
-        string path = dir + mask;
+        string path = a + m;
 
         if (path.empty()) return;
 
@@ -224,86 +118,93 @@ public:
 
                 if (d.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 
-                    for (unsigned int j = 0; j < depth; j++) cout << "  ";
+                    for (unsigned int j = 0; j < n; j++) cout << "  ";
 
-                    cout << dir << d.cFileName << endl;
-                    string new_path = dir + d.cFileName + "\\";
+                    cout << a << d.cFileName << endl;
+                    string new_path = a + d.cFileName + "\\";
 
-                    my_search_tree(new_path, mask, depth + 1);
+                    Search(new_path, m, n + 1);
 
                 }
                 else {
 
-                    for (unsigned int j = 0; j < depth; j++) cout << "  ";
+                    for (unsigned int j = 0; j < n; j++) cout << "  ";
 
                     cout << "   " << d.cFileName << endl;
                 }
             }
         } while (FindNextFileA(h, &d));
     }
- 
+
+    void move()
+    {
+        cout << "For example ->>\nfor file->> move D:\\rtff* D:\\Dota\nfor folder->>move D:\\Folder D:\\Folder2\n";
+        cout << "Enter path and name move to file: ";
+        string mo;
+        getline(cin, mo);
+        char* m= _strdup(mo.c_str());
+        system(m);
+    }
 };
 
 int main(void)
 {
     Papki Pa;
-   /* Pa.CreatePapka();
-    Pa.DelPapka();*/
-    /*Pa.CreatePapka();*/
-    /*Pa.Search();*/
-    /*Pa.Disk2();*/
-   /* Pa.Inf();*/
-    /*Pa.CreatePapka();*/
-    string path = "D:\\Lineage 2\\";
-    cout << "START: " << path << endl;
-
-    Pa.my_search_tree(path, "*.*", 0);
-   /* Pa.Inform();*/
-    //char curdir[MAXPATH]; //объявляем строку
-
-    //Pa.GetCurrentDir(curdir); //записываем в строку текущий путь
-
-    //printf("\nТекущая папка %s", curdir);
-
-    ////переходим на диск С, в корневой каталог.
-    //chdir("c:\\");
-
-    ////создаём папку c названием: "freedom"
-    //mkdir("freedom");
-
-    ////возвращаемся назад
-    //chdir(curdir);
-
-    /*vector<string>file;
-    file.push_back("1.bmp");
-    Pa.dfsFolder("D:", file);*/
-    
-   
-  /*  WIN32_FIND_DATA FindFileData = FILE_ATTRIBUTE_DIRECTORY;
-    HANDLE hFind;
-
-    hFind = FindFirstFile("*", &FindFileData);
-    if (hFind == INVALID_HANDLE_VALUE)
-        return 1;
-    else
+    string path;
+    char vvod;
+    do
     {
-        cout << FindFileData.cFileName << endl;
-        FindClose(hFind);
-        return 0;
-    }*/
-   /* Pa.TEST2();*/
+        system("cls");
+        cout << "1 - Search\n";
+        cout << "2 - Add folder\n";
+        cout << "3 - Del folder\n";
+        cout << "4 - Rename\n";
+        cout << "5 - Name disk\n";
+        cout << "6 - move file and folder\n";
+        cout << "Exit - Esc\n";
+        vvod = _getch();
+        switch (vvod)
+        {
+        case'1':
+        {
+            cout<< "for example ->>D:\\Lineage 2\\\tor\t->>D:\\\n";
+            cout << "START: ";
+            getline(cin,path);
+            Pa.Search(path, "*.*", 0);
+            system("pause");
+            break;
+        }
+        case'2':
+        {
+            Pa.CreatePapka();
+            system("pause");
+            break;
+        }
+        case'3':
+        {
+            Pa.DelPapka();
+            system("pause");
+            break;
+        }
+        case'4':
+        {
+            Pa.Rename(); //tak je mojno menat' format faila
+            system("pause");
+            break;
+        }
+        case'5':
+        {
+            Pa.Disk2();
+            system("pause");
+            break;
+        }
+        case'6':
+        {
+            Pa.move();
+            system("pause");
+            break;
+        }
+        }
 
-  
-
-    /*if (_mkdir("\\testtmp") == 0)
-    {
-        printf("Directory '\\testtmp' was successfully created\n");
-        system("dir \\testtmp");
-        if (_rmdir("\\testtmp") == 0)
-            printf("Directory '\\testtmp' was successfully removed\n");
-        else
-            printf("Problem removing directory '\\testtmp'\n");
-    }
-    else
-        printf("Problem creating directory '\\testtmp'\n");*/
+    } while (vvod != 27);
 }
